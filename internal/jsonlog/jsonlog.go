653c06245385036bf5d8d2f76a19dfc0b825fadf
guths <jonathanguths@gmail.com>
2023-10-25 09:41:21 -0300

    init

---
+++ internal/jsonlog/jsonlog.go
@@ -0,0 +1,114 @@
+package jsonlog
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+	"runtime/debug"
+	"sync"
+	"time"
+)
+
+type Level int8
+
+const (
+	LevelInfo Level = iota
+	LevelError
+	LevelFatal
+	LevelOff
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelInfo:
+		return "INFO"
+	case LevelFatal:
+		return "FATAL"
+	case LevelError:
+		return "ERROR"
+	default:
+		return ""
+	}
+}
+
+type Logger struct {
+	out      io.Writer
+	minLevel Level
+	mu       sync.Mutex
+	env      string
+}
+
+func New(env string, out io.Writer, minLevel Level) *Logger {
+	if env == "development" {
+		out, _ = createLocalLog()
+	}
+
+	return &Logger{
+		minLevel: minLevel,
+		out:      out,
+		env:      env,
+	}
+}
+
+func createLocalLog() (*os.File, error) {
+	f, err := os.Create("logs/logs.txt")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+func (l *Logger) PrintInfo(message string, properties map[string]string) {
+	l.print(LevelInfo, message, properties)
+}
+
+func (l *Logger) PrintError(err error, properties map[string]string) {
+	l.print(LevelError, err.Error(), properties)
+}
+
+func (l *Logger) PrintFatal(err error, properties map[string]string) {
+	l.print(LevelFatal, err.Error(), properties)
+	os.Exit(1)
+}
+
+func (l *Logger) print(level Level, message string, properties map[string]string) (int, error) {
+	if level < l.minLevel {
+		return 0, nil
+	}
+
+	aux := struct {
+		Level      string `json:"level"`
+		Time       string `json:"time"`
+		Message    string `json:"message"`
+		Properties map[string]string
+		Trace      string `json:"trace"`
+	}{
+		Level:      level.String(),
+		Time:       time.Now().Format(time.RFC3339),
+		Message:    message,
+		Properties: properties,
+	}
+
+	if level >= LevelError {
+		aux.Trace = string(debug.Stack())
+	}
+
+	var line []byte
+
+	line, err := json.Marshal(aux)
+
+	if err != nil {
+		line = []byte(LevelError.String() + ": unable to marshel log message: " + err.Error())
+	}
+
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	return l.out.Write(append(line, '\n'))
+}
+
+func (l *Logger) Write(message []byte) (n int, err error) {
+	return l.print(LevelError, string(message), nil)
+}
